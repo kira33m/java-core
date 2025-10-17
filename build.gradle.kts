e0a7b5d6189dd8f7bcbdb074ef807ed8bbbf65fa
Kira <tazimuratovalmaz12@gmail.com>
2025-10-17 17:30:01 +0500

    logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.projectlombok:lombok:1.18.34")
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
 }
 
 tasks.test {
